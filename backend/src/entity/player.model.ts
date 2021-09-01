98e6aea92c5151f5107c1e3078741c72f896e12b
sthatrab <haythem.trabelsi@stud.h-da.de>
2021-09-01 22:55:33 +0200

    fix: player.model conflict

--- backend/src/entity/player.model.ts
+++ backend/src/entity/player.model.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
     Entity,
     PrimaryGeneratedColumn,
@@ -76,42 +75,3 @@ import {
    */
   }
   
-=======
-import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import * as yup from "yup";
-import { Game } from "./game.model";
-import { Move } from "./move.model";
-
-export const playerSchema = yup.object().shape({
-  name: yup.string().required(),
-  description: yup.string().required(),
-  //....
-});
-
-@Entity()
-export class Player {
-  @PrimaryGeneratedColumn("uuid")
-  id!: string;
-
-  @Column()
-  name!: string;
-
-  @Column()
-  password!: string;
-  
-  @Column()
-  email!: string;
-
-  @ManyToMany (type => Game, game => game.players)
-  games!: Game[];
-
-  @OneToMany (type => Move, move => move.player)
-  moves!: Move[];
-
-  /* @Column()
-  settings!: Array<string>;
-
-  @Column()
-  score!: Array<string>; */
-}
->>>>>>> a6bc9b62a73734ceffee1220f4933ecb5573ad1a
