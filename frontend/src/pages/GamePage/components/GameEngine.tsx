d3c6ae355178978440b8a8fc4e16468899b99e98
Felix Chr <felix.c.k.christians@stud.h-da.de>
2021-09-01 04:31:54 +0200

    feat: implement timed mode support
    - define time in ms for player
    - define time bonus in ms for player to get for adding a piece to the board

--- frontend/src/pages/GamePage/components/GameEngine.tsx
+++ frontend/src/pages/GamePage/components/GameEngine.tsx
@@ -17,6 +17,7 @@ export enum GameStateEnum {
 export interface GameState {
     winner: string | undefined;
     active_player: string | undefined;
+    active_start: Date | undefined;
     players: Map<string, Player>;
     state: GameStateEnum;
     steps: GameStep[];
@@ -26,6 +27,7 @@ export interface Player {
     name: string;
     color: string;
     time: number;
+    time_bonus: number;
 }
 
 interface Direction {
@@ -104,15 +106,17 @@ export class game {
      * @param id : string
      * @param name : string
      * @param color : string
-     * @param time : number
+     * @param time : number - Total time in ms
+     * @param time_bonus : number (optional) - Default 3000(ms)
      */
-    addPlayer(id: string, name: string, color: string, time: number) {
+    addPlayer(id: string, name: string, color: string, time: number, time_bonus: number = 3000) {
         this.state.players.set(
             id,
             {
                 name: name,
                 color: color,
-                time: time
+                time: time,
+                time_bonus: time_bonus,
             } as Player);
     }
 
@@ -127,7 +131,6 @@ export class game {
     }
 
     private create_step(x: number, y: number): GameStep {
-        this.check_active_player();
         return {
             player_id: this.state.active_player!,
             color: this.state.players.get(this.state.active_player!)!.color,
@@ -160,16 +163,39 @@ export class game {
         } else {
             this.state.active_player = id;
             this.state.state = GameStateEnum.IN_PROGRESS;
+            this.state.active_start = new Date();
         }
     }
 
     /**
-     * Attempts to insert a piece in the given column.
-     * Returns GameStep when successful or undefined if failed
+     * Sets active_player to undefined and performs game-over checks.
      *
-     * Sets active_player to undefined
-     * Checks if game-over conditions have been met
+     * @param add_bonus_time : boolean - Decides whether to add the time_bonus defined for the active player.
+     */
+    stopPlayer(add_bonus_time: boolean = false) {
+        if (this.state.active_player !== undefined) {
+            let time_bonus = add_bonus_time ? this.state.players.get(this.state.active_player!)!.time_bonus : 0;
+            this.state.players.get(this.state.active_player!)!.time +=
+                time_bonus -
+                (new Date().getTime() - this.state.active_start!.getTime());
+            this.has_winner(
+                this.state.steps[this.state.steps.length-1].x,
+                this.state.steps[this.state.steps.length-1].y
+            );
+            this.state.active_player = undefined;
+            this.state.active_start = undefined;
+        }
+    }
+
+    /**
+     * Attempts to insert a piece in the given column as the active player.
+     * Returns GameStep when successful or undefined if failed.
+     *
+     * Sets active_player to undefined.
+     * Checks if game-over conditions have been met.
      * @param column : number
+     *
+     * @exception Error if no active player is set.
      *
      * @return GameStep | undefined
      */
@@ -177,6 +203,7 @@ export class game {
         if (this.state.state !== GameStateEnum.IN_PROGRESS) {
             return undefined;
         }
+        this.check_active_player();
 
         let row: number = 0;
         while (this.game_board[column][row] !== 0) {
@@ -188,13 +215,16 @@ export class game {
         this.state.steps.push(this.create_step(column, row));
         this.game_board[column][row] = this.state.steps.length - 1;
 
-        this.has_winner(column, row);
-        this.state.active_player = undefined;
+        this.stopPlayer(true);
 
         return this.state.steps[this.state.steps.length - 1];
     }
 
-    private has_winner(x: number, y: number, player_id: string | undefined = this.state.active_player) {
+    private has_winner(
+        x: number,
+        y: number,
+        player_id: string = this.state.steps[this.state.steps.length-1].player_id)
+    {
         this.directions.forEach((direction) => {
             if (this.state.steps.length + 1 === this.width * this.height) {
                 this.state.state = GameStateEnum.TIE;
@@ -215,9 +245,7 @@ export class game {
                 xi += direction.x;
                 yi += direction.y;
             }
-        })
-
-
+        });
     }
 
     private static in_range(x: number, exclMax: number, incMin: number = 0) {
